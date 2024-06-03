c7e205d2bc825ad43148b5430a8e84d31a9e51ab
Jaime Muñoz <jaime.munozq@udea.edu.co>
2024-06-03 11:43:24 -0500

    set initial configuration

---
+++ src/main/java/com/udea/parcialarquisoft/ParcialarquisoftApplication.java
@@ -0,0 +1,13 @@
+package com.udea.parcialarquisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialarquisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialarquisoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/parcialarquisoft/ParcialarquisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.parcialarquisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialarquisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
